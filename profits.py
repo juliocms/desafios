e76c45121bb3c99a948824f47d25b3999677acad
juliocms@gmail.com <juliocms@gmail>
2023-06-05 19:49:35 -0300

    Create class and method

--- profits.py
+++ profits.py
@@ -1,4 +1,9 @@
-transacoes = [
+class AcquirerLTDA:
+
+    def profit_calculate(self, transactions, contracts):
+        return 0
+
+transactions = [
     {"transaction_id": 1, "client_id": 3545, "total_amount": 3000, "discount_percentage": 6.99},
     {"transaction_id": 2, "client_id": 3545, "total_amount": 4500, "discount_percentage": 0.45},
     {"transaction_id": 3, "client_id": 3509, "total_amount": 69998, "discount_percentage": 0},
@@ -6,9 +11,15 @@ transacoes = [
     {"transaction_id": 5, "client_id": 4510, "total_amount": 34, "discount_percentage": 40},
 ]
 
-contratos = [
+contracts = [
     {"contract_id": 3, "client_id": 3545, "client_name": "Magazine Luana", "percentage": 2.00, "is_active": True},
     {"contract_id": 4, "client_id": 3545, "client_name": "Magazine Luana", "percentage": 1.95, "is_active": False},
     {"contract_id": 5, "client_id": 3509, "client_name": "Lojas Italianas", "percentage": 1, "is_active": True},
     {"contract_id": 6, "client_id": 3510, "client_name": "Carrerfive", "percentage": 3.00, "is_active": True},
 ]
+
+
+acquirer = AcquirerLTDA()
+profit = acquirer.profit_calculate(transactions, contracts)
+
+print(f"The profit of the company Acquirer LTDA is {profit}")
